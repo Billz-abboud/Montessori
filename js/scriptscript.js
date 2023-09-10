0282d723a8d3713f74ef236faf7b91a08df78207
Bilal Abboud <52608994+Billz-abboud@users.noreply.github.com>
2023-09-10 12:11:22 +0300

    Update scriptscript.js

--- js/scriptscript.js
+++ js/scriptscript.js
@@ -1,5 +1,31 @@
+var objPeople = [
+	{ // Object @ 0 index
+		username: "bilal",
+		password: "abboud"
+	},
+	{ // Object @ 1 index
+		username: "zainab",
+		password: "safawi"
+	}
+]
 
-var usernames = ["smith",  "tron",      "ace",      "ladyj",    "anon"];
+function getInfo() {
+	var username = document.getElementById('username').value
+	var password = document.getElementById('password').value
+
+	for(var i = 0; i < objPeople.length; i++) {
+		// check is user input matches username and password of a current index of the objPeople array
+		if(username == objPeople[i].username && password == objPeople[i].password) {
+			console.log(username + " is logged in!!!")
+			// stop the function if this is found to be true
+            window.location.href = 'http://www.google.com';
+			return
+            
+		}
+	}
+	console.log("incorrect username or password")
+}
+/*var usernames = ["smith",  "tron",      "ace",      "ladyj",    "anon"];
 var passwords = ["qwerty", "EndOfLine", "year1942", "ladyj123", "PASSWORD"];
 
 var x = prompt("Please enter the username");
@@ -24,4 +50,4 @@ if(valid) {
 }
 else {
   alert("invalid credentials");
-}
+}*/
